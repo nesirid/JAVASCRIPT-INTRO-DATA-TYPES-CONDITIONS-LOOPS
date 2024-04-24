4253eaa9983d4a04ce08263661978422ed833466
nesirid <nasirid@code.edu.az>
2024-04-24 22:43:28 +0400

    HomeTask

---
+++ assets/js/script.js
@@ -0,0 +1,197 @@
+"use strict";
+
+// console.log("Salam Elmir bey");
+
+// var adrress="Ehmedli";
+// var adrress="gence"
+// console.log(address);
+// adrress=55;
+// var isMarried=true;
+
+// let phone=324674763;
+// const email="test@gmail.com";
+// console.log(email);
+
+// console.log(100+100);
+
+// surname="test";
+
+
+// let a = 5;
+// let b = 10;
+// let result = a + b;
+// a++;
+// console.log(result);
+
+// let a = 5;
+// a++;
+// console.log(a/0);
+
+
+
+// let isMarried = true;
+// let number = 200;
+
+
+// if(isMarried == true)
+// {
+// console.log("Evlidir");
+// }
+// else
+// {
+//     console.log("Subaydir");
+// }
+
+
+// let number = 100;
+
+// if(number > 50 && number < 150)
+// {
+// console.log("yes")
+// }
+// else
+// {
+//     console.log("no")
+// }
+
+
+// ____________________________________________
+
+
+// let weekDay = "Wednesday";
+// switch(weekDay)
+// {
+//     case "Tuesday":
+//         console.log("Tuesday")
+//         break;
+//         case "Wednesday":
+//             console.log("Wednesday")
+//             break;
+//             default:
+//                 console.log("Not selected")
+//                 break
+// }
+
+
+// alert("Welcome to group")
+// let result = confirm("Press a button!");
+// console.log(result)
+// let person = prompt("Please enter your name", "Harry Potter");
+
+// if(person == null)
+// {
+//     console.log("Operation canceled")
+// }
+// else
+// {
+//     console.log(person)
+// }
+
+
+
+
+// let a = 5;
+// while(a<8)
+// {
+//     console.log(a);
+//     a++;
+// }
+
+// for(let i=0;i<50;i++){
+//     console.log(i);
+// }
+
+
+
+// let a =5;
+// let b=a**2;
+
+
+
+// console.log(b);
+
+// let num1=100;
+// let num2="100";
+// console.log(num1==num2);
+
+// let datas=[1,2,3,"salam",true];
+
+// console.log(datas[3]);
+
+// for (let i =0 ; i< datas.lengt;i++) {
+
+//     console.log(datas[i]);
+    
+// }
+
+
+
+
+
+// for (let i =0 ; i< datas.lengt;i++){
+//     if(!isNaN (datas[i])){
+
+//         console.log(datas[i]);
+
+//     }        
+        
+// }    
+
+
+
+
+
+
+
+// let isActive=true;
+// console.log(typeof isActive=="boolean");
+// let datas=[1,2,3,"salam",true];
+// for (let i =0 ; i< datas.length;i++){
+//         if(!isNaN (datas[i]) && typeof datas[i]!="boolean"){
+    
+//             console.log(datas[i]);
+    
+//         }                   
+    // }    
+
+
+
+
+    // let elem=50;
+    // let result=elem>100 ? "yes" :"No";
+    // console.log(result);
+
+//     let a = null;
+//     let b ;
+// console.log(b);
+
+// let numbers=[30,6,8,9,15,11];
+// let sum=0;
+// for (let i =0 ; i< numbers.length;i++){
+//     sum+=numbers[i];
+// }
+    
+// console.log(sum);
+
+
+
+// let isTrue=true;
+// if(isTrue){
+//     var name="Fexriyye"
+// }
+// console.log(name);
+
+
+
+const products=["mono","hello"];
+for (let i =0 ; i< products.length;i++){
+    console.log(products[i]);
+}
+
+
+
+
+
+const datas=[1,2,3,4,5];
+datas[2]=100;
+console.log(datas);
